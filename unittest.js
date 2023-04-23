616ab50f258d72af020b776cd38052451f64cac1
Tensh1row <113771106+Tensh1row@users.noreply.github.com>
2023-04-23 23:43:29 +0300

    Update unittest.js

--- unittest.js
+++ unittest.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 describe('GET /users/:userId/todos', () => {
     it('should return a list of todos for a user', async () => {
-        // Arrange
+      
         const userId = 1;
         const expectedTodoList = [
             {
@@ -19,25 +19,25 @@ describe('GET /users/:userId/todos', () => {
                 "title": "quis ut nam facilis et officia qui",
                 "completed": false
             },
-            // Add more expected todos here
+         
         ];
 
-        // Act
+   
         const response = await axios.get(`http://localhost:3000/users/${userId}/todos`);
 
-        // Assert
+     
         assert.deepStrictEqual(response.status, 200);
         assert.deepStrictEqual(response.data, expectedTodoList);
     });
 
     it('should return 500 Internal Server Error if there is an error with the request', async () => {
-        // Arrange
-        const userId = 999; // invalid user ID
+      
+        const userId = 999; 
 
-        // Act
+   
         const response = await axios.get(`http://localhost:3000/users/${userId}/todos`);
 
-        // Assert
+   
         assert.deepStrictEqual(response.status, 500);
         assert.deepStrictEqual(response.data, 'Internal Server Error');
     });
@@ -45,34 +45,34 @@ describe('GET /users/:userId/todos', () => {
 
 describe('POST /users/:userId/todos', () => {
     it('should create a new todo for a user', async () => {
-        // Arrange
+   
         const userId = 1;
         const todoData = {
             title: 'test todo',
             completed: false
         };
 
-        // Act
+    
         const response = await axios.post(`http://localhost:3000/users/${userId}/todos`, todoData);
 
-        // Assert
+      
         assert.deepStrictEqual(response.status, 200);
         assert.deepStrictEqual(response.data.title, todoData.title);
         assert.deepStrictEqual(response.data.completed, todoData.completed);
     });
 
     it('should return 500 Internal Server Error if there is an error with the request', async () => {
-        // Arrange
-        const userId = 999; // invalid user ID
+      
+        const userId = 999; 
         const todoData = {
             title: 'test todo',
             completed: false
         };
 
-        // Act
+      
         const response = await axios.post(`http://localhost:3000/users/${userId}/todos`, todoData);
 
-        // Assert
+      
         assert.deepStrictEqual(response.status, 500);
         assert.deepStrictEqual(response.data, 'Internal Server Error');
     });
